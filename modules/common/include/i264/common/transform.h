b7c53875b7681d8abd10ca90025dad6e5cb83c6a
Caoyang Jiang <cjiang@mtu.edu>
2020-03-20 12:58:33 -0700

    - add luma DC transform

--- modules/common/include/i264/common/transform.h
+++ modules/common/include/i264/common/transform.h
@@ -20,6 +20,14 @@ class Transform {
  public:
   /**
    * @brief This applies to residual of all 4x4 block
+   *  ouptut = Cf * input * transpose(Cf)
+   *  where Cf is
+   *  1  1  1  1
+   *  2  1 -1 -2
+   *  1 -1 -1  1
+   *  1 -2  2 -1
+   *
+   *  (note no scaling, quantizing of any sort is implemented here.)
    *
    * @tparam BLOCK_WIDTH Input block width. Must be multiple of 4.
    * @tparam BLOCK_HEIGHT Input block height. Must be multiple of 4.
@@ -55,6 +63,14 @@ class Transform {
 
   /**
    * @brief This applies to dct-transformed coefficients of all 4x4 block
+   * *  ouptut = transpose(Ci) * input * Ci
+   *  where Cf is
+   *  1   1     1   1
+   *  1  1/2  -1/2 -1
+   *  1   -1    -1  1
+   *  1/2 -1    1 -1/2
+   *
+   *  (note no scaling, quantizing of any sort is implemented here.)
    *
    * @tparam BLOCK_WIDTH Input block width. Must be multiple of 4.
    * @tparam BLOCK_HEGIHT Input block height. Must be multiple of 4.
@@ -74,7 +90,7 @@ class Transform {
    * @param second_trans_dc_coeff hadamard transformed coefficients.
    * @param first_trans_dc_coeff dct coefficients from the initial 4x4 transform
    */
-  static void Invers4x4LumaDC(
+  static void Inverse4x4LumaDC(
       const Array2D<int32_t, 4, 4>& second_trans_dc_coeff,
       Array2D<int32_t, 16, 16>& first_trans_dc_coeff);
 
@@ -212,10 +228,109 @@ void Transform::Inverse4x4Residual(
       // assign coefficents
       for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-          output[pixel_y + i][pixel_x + j] = (h[i][j] + (1 << 5)) >> 6;
+          output[pixel_y + i][pixel_x + j] = h[i][j];
         }
       }
+
+      // for (int i = 0; i < 4; i++) {
+      //   for (int j = 0; j < 4; j++) {
+      //     output[pixel_y + i][pixel_x + j] = (h[i][j] + (1 << 5)) >> 6;
+      //   }
+      // }
     }
+  }
+}
+
+void Transform::Forward4x4LumaDC(const Array2D<int32_t, 16, 16>& input,
+                                 Array2D<int32_t, 4, 4>& output) {
+  // Defined in 8.5.10
+
+  // horizontal transform
+  // compute all e's
+  Array2D<int32_t, 4, 4> e;
+  for (int i = 0; i < 4; i++) {
+    e[0][i] = input[0][4 * i] + input[12][4 * i];
+    e[1][i] = input[0][4 * i] - input[12][4 * i];
+    e[2][i] = input[4][4 * i] + input[8][4 * i];
+    e[3][i] = input[4][4 * i] - input[8][4 * i];
+  }
+
+  // compute all f's
+  Array2D<int32_t, 4, 4> f;
+  for (int i = 0; i < 4; i++) {
+    f[0][i] = e[0][i] + e[2][i];
+    f[1][i] = e[1][i] + e[3][i];
+    f[2][i] = e[0][i] - e[2][i];
+    f[3][i] = e[1][i] - e[3][i];
+  }
+
+  // vertical transform
+  // compute all g's
+  Array2D<int32_t, 4, 4> g;
+  for (int i = 0; i < 4; i++) {
+    g[i][0] = f[i][0] + f[i][3];
+    g[i][1] = f[i][0] - f[i][3];
+    g[i][2] = f[i][1] + f[i][2];
+    g[i][3] = f[i][1] - f[i][2];
+  }
+
+  // compute all h's
+  for (int i = 0; i < 4; i++) {
+    output[i][0] = g[i][0] + g[i][2];
+    output[i][1] = g[i][1] + g[i][3];
+    output[i][2] = g[i][0] - g[i][2];
+    output[i][3] = g[i][1] - g[i][3];
+  }
+}
+
+void Transform::Inverse4x4LumaDC(const Array2D<int32_t, 4, 4>& input,
+                                 Array2D<int32_t, 16, 16>& output) {
+  // Defined in 8.5.10
+
+  // horizontal transform
+  // compute all e's
+  Array2D<int32_t, 4, 4> e;
+  for (int i = 0; i < 4; i++) {
+    e[0][i] = input[0][i] + input[3][i];
+    e[1][i] = input[0][i] - input[3][i];
+    e[2][i] = input[1][i] + input[2][i];
+    e[3][i] = input[1][i] - input[2][i];
+  }
+
+  // compute all f's
+  Array2D<int32_t, 4, 4> f;
+  for (int i = 0; i < 4; i++) {
+    f[0][i] = e[0][i] + e[2][i];
+    f[1][i] = e[1][i] + e[3][i];
+    f[2][i] = e[0][i] - e[2][i];
+    f[3][i] = e[1][i] - e[3][i];
+  }
+
+  // vertical transform
+  // compute all g's
+  Array2D<int32_t, 4, 4> g;
+  for (int i = 0; i < 4; i++) {
+    g[i][0] = f[i][0] + f[i][3];
+    g[i][1] = f[i][0] - f[i][3];
+    g[i][2] = f[i][1] + f[i][2];
+    g[i][3] = f[i][1] - f[i][2];
+  }
+
+  // compute all h's
+  Array2D<int32_t, 4, 4> h;
+  for (int i = 0; i < 4; i++) {
+    h[i][0] = g[i][0] + g[i][2];
+    h[i][1] = g[i][1] + g[i][3];
+    h[i][2] = g[i][0] - g[i][2];
+    h[i][3] = g[i][1] - g[i][3];
+  }
+
+  // assign coefficents
+  for (int i = 0; i < 4; i++) {
+    output[0][4 * i] = h[0][i];
+    output[4][4 * i] = h[1][i];
+    output[8][4 * i] = h[2][i];
+    output[12][4 * i] = h[3][i];
   }
 }
 
